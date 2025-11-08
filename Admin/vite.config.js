594c66c21740ff2e904a1a473f4b1c7c19974578
kishore7772005 <kishoreajith777@gmail.com>
2025-11-08 17:48:57 +0530

    commit

--- Admin/vite.config.js
+++ Admin/vite.config.js
@@ -3,13 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => ({
   plugins: [
-    react({
-      fastRefresh: mode === 'development',  // Enable only in dev
-    }),
+    
   ],
   server: {
     port: 5000,
     host: '0.0.0.0',
-    allowedHosts: ['insurance-project-admin.onrender.com'],
+    allowedHosts: ['insurance-project-u9wk.onrender.com'],
   },
 }))
