3d9bb28a3bfdcaf974fbaa2778a27ae113181dbb
kishore7772005 <kishoreajith777@gmail.com>
2025-11-08 17:39:54 +0530

    commit

--- Admin/vite.config.js
+++ Admin/vite.config.js
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-   server : {
-    port: 5000 , 
-    host: '0.0.0.0' , 
-    allowedHosts: [ 'insurance-project-admin.onrender.com' ]
-  }
-})
+export default defineConfig(({ mode }) => ({
+  plugins: [
+    react({
+      fastRefresh: mode === 'development',  // Enable only in dev
+    }),
+  ],
+  server: {
+    port: 5000,
+    host: '0.0.0.0',
+    allowedHosts: ['insurance-project-admin.onrender.com'],
+  },
+}))
