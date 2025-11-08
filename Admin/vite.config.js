bc534cc62b64ff1ba05d5a8884ef9e1b1b00b38a
kishore7772005 <kishoreajith777@gmail.com>
2025-11-08 17:49:43 +0530

    nkdjf

--- Admin/vite.config.js
+++ Admin/vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => ({
-  plugins: [
-    
-  ],
-  server: {
-    port: 5000,
-    host: '0.0.0.0',
-    allowedHosts: ['insurance-project-u9wk.onrender.com'],
-  },
-}))
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+   server : {
+    port: 5000 , 
+    host: '0.0.0.0' , 
+    allowedHosts: [ 'insurance-project-u9wk.onrender.com' ]
+  }
+})
